d7494d89e0fb714dd269f53276767e89f65625ad
marisolmaldonado <magaly.mary.98@gmail.com>
2021-01-17 20:11:54 -0500

    Add files via upload

---
+++ IGame.cs
@@ -0,0 +1,16 @@
+﻿
+namespace UnityExample
+{
+    public interface IGame
+    {
+        string Name { get; }
+        int CurrentPlayers { get;  }
+        int MinPlayers { get;  }
+        int MaxPlayers { get; }
+        void AddPlayer();
+        void RemovePlayer();
+        void Play();
+        string Result();
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Unity;
+
+namespace UnityExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var unityContainer = new UnityContainer();
+            unityContainer.RegisterType<IGame, Trivial>();
+            var game = unityContainer.Resolve<IGame>();
+            game.AddPlayer();
+            game.AddPlayer();
+            Console.WriteLine(string.Format("{0} Personas jugando a {1}",
+                game.CurrentPlayers, game.Name));
+            Console.ReadLine();
+            var table = unityContainer.Resolve<Table>();
+            table.Play();
+
+            Console.WriteLine(table.GameStatus());
+            Console.ReadLine();
+        }
+    }
+    Console.WriteLine("precione Escape para detener");
+    ConsoleKey tecla;
+
+    do
+    {
+    while(!Console.KeyAvailable)
+        {
+        //toDo movimiento del auto
+        }
+tecla = Console.ReadKey(true).Key;
+// HACER ALGO CON LA TECLA : DIRECION DEL AUTO
+}
+while (tecla != ConsoleKey.Escape) ;
+
+}
+
+
+
+
